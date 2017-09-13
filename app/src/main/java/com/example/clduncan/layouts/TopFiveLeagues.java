e16a5d74bef7e564dd0380a9ae7993bc6982921c
ChrisDunc <christopher.duncan@stu.fayette.kyschools.us>
2017-09-13 10:34:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/clduncan/layouts/TopFiveLeagues.java
@@ -0,0 +1,13 @@
+package com.example.clduncan.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopFiveLeagues extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_five_leagues);
+    }
+}
